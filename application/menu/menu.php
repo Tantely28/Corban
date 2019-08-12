012e5f67ab39ec0b66986129f754db4c700d6a86
SarobidyToky <hastma12@gmail.com>
2019-08-12 14:22:00 +0100

    Ajout du formulaire connexion client et candidat

--- application/menu/menu.php
+++ application/menu/menu.php
@@ -13,9 +13,73 @@
             <li class="nav-item"><a href="index.php?page=service" class="nav-link">Service</a></li>
             <li class="nav-item"><a href="index.php?page=offres" class="nav-link">Nos offres d'emploi</a></li>
             <li class="nav-item"><a href="index.php?page=apropos" class="nav-link">A propos</a></li>
-            <li class="nav-item"><a href="index.php?page=connexion" class="nav-link">Connexion</a></li>
+
+            <li class="nav-item">
+              <a href="index.php?page=connexion" data-toggle="modal" data-target="#modalConnexion" class="nav-link">Connexion</a>
+            </li>
+
             <li class="nav-item"><a href="index.php?page=contact" class="nav-link">Contact</a></li>
           </ul>
         </div>
       </div>
     </nav>
+
+    <!-- MODAL -->
+    <div class="modal fade" id="modalConnexion">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">Connecter en tant que : </div>
+                <div class="modal-body text-center">
+                    <button data-toggle="modal" data-target="#modalClient" class="btn btn-primary">Client</button>
+                    <button class="btn btn-primary" data-toggle="modal" data-target="#modalCandidat">Candidat</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- MODAL CLIENT-->
+    <div class="modal fade" id="modalClient">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">Connexion</div>
+                <div class="modal-body">
+                    <form action="#" method="post">
+                        <div class="form-group">
+                            <label for="email">Adresse email</label>
+                            <input type="email" class="form-control" id="email" placeholder="Votre adresse email...">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Mot de passe</label>
+                            <input type="password" class="form-control" id="password" placeholder="Votre mot de passe...">
+                        </div>
+                    </form>
+                    <button type="submit" class="btn btn-warning">Se connecter</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- MODAL CANDIDAT-->
+    <div class="modal fade" id="modalCandidat">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">Connexion</div>
+                <div class="modal-body">
+                    <form action="#" method="post">
+                        <div class="form-group">
+                            <label for="email">Adresse email</label>
+                            <input type="email" class="form-control" id="email" placeholder="Votre adresse email...">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Mot de passe</label>
+                            <input type="password" class="form-control" id="password" placeholder="Votre mot de passe...">
+                        </div>
+                    </form>
+                    <button type="submit" class="btn btn-warning">Se connecter</button>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-info">S'inscrire</button>
+                </div>
+            </div>
+        </div>
+    </div>
