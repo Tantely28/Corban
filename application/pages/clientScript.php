292f67bf0c71d640a521df14c3bd66e6db406d01
SarobidyToky <hastma12@gmail.com>
2019-08-20 23:05:06 +0100

    Temoignage video

--- application/pages/clientScript.php
+++ application/pages/clientScript.php
@@ -35,7 +35,7 @@
                     if (response.message == null){
                         // document.cookie = "Id" + response.id;
                         // document.getElementById("idSession").value = response.id;
-                        window.location.href = "http://localhost/projet/corban/Corban/application/index.php?page=home&user="+response.user+"&id="+response.id;
+                        window.location.href = "http://localhost/Corban/application/index.php?page=home&user="+response.user+"&id="+response.id;
                     } else {
                         alert(response.message);
                     }
