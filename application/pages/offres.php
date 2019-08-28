269c234cf5f1981f2a4a1f7648a698b6558abe5d
Mharison <Rakt4.co@gmail.com>
2019-08-28 15:18:07 +0200

    m

--- application/pages/offres.php
+++ application/pages/offres.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Nos offres d'emploi</title>
-
+	<link href="https://fonts.googleapis.com/css?family=Arimo:400,700|Lora:400,700|Raleway:400,700|Roboto:400,700&display=swap" rel="stylesheet"> 
 <style>
 	.btn-5{
 		border-radius: none;
@@ -41,25 +41,6 @@
 		background: #ffffff;
 		border:2px solid #3d5a98;
 	}
-	.btn-8{
-  text-align: center;
-  font-weight: bold;
-  padding: 10px 40px;
-  font-size: 30px
-  color: #545454;
-  background: #ffffff;
-  border-radius: 50px;
-  border: none;
-}
-.btn-8:hover{
-  font-weight: bold;
-  padding: 10px 40px;
-  font-size: 30px
-  border-radius: 50px;
-  background:#545454;
-  border:2px solid #fff;
-  color: #fff;
-}
 </style>
 </head>
 <body>
@@ -81,85 +62,14 @@
     	</div>
     </div>	
 </div>
-<div id="main">
-
-</div>
 <div class="container-fluid" style="background-color: #f7f7f7;padding: 80px 20px;">
 	<div class="container">
-		<div class="row" style="color: #ffffff;">
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" >
-				<div style="background-color:#3b4a61 ;padding:10px 10px;">
-					<p style="margin-top: 80px;font-size: 15px;">
-					Corban Performance Consulting
-				</p>
-				<h2 style="font-weight: bold;
-						   color: #ffffff;
-						   text-align: center; 
-						   font-size:40px;">
-					Business <br>Partner RH
-				</h2>
-				<p style="margin-top: 10px;font-size: 12px;text-align: right;">
-					pour VIVETIC
-				</p>
-				<div style="margin-top: 85px;
-				           font-weight: bold;
-				           text-align: right;">
-					<a href="#" style="color: #ffffff;text-decoration: none;">
-						Détails
-					</a>
-				</div>
-				</div>
-				
-			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" >
-				<div style="background-color:#652150 ;padding:10px 10px;">
-					<p  style="margin-top: 80px;font-size: 15px">
-					Corban Performance Consulting
-				</p>
-				<h2 style="font-weight: bold;
-						    color: #ffffff;
-						    text-align: center; 
-						    font-size:40px;">
-					Senior Programme Manager
-				</h2>
-				<div style="margin-top: 75px;
-					        font-weight: bold;
-					        text-align: right;">
-					<a href="#" style="color: #ffffff;text-decoration: none;">
-						Détails
-					</a>
-				</div>
-				</div>
-				
-			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" >
-				<div style="background-color:#3b4a61 ;padding:10px 10px;">
-					<p  style="margin-top: 80px;font-size: 15px">
-					Corban Performance Consulting
-				</p>
-				<h2 style="font-weight: bold;
-							color: #ffffff;
-							text-align: center; 
-							font-size:40px;">
-					Lead <br>Qualifiers
-				</h2>
-				<div style="margin-top: 114px;
-							font-weight: bold;
-							text-align: right;">
-					<a href="#"  style="color: #ffffff;text-decoration: none;">
-						Détails
-					</a>
-				</div>
-				</div>
-				
-			</div>
-		</div>
-
+		<div class="row" style="color: #ffffff;"id="main" >
+			
+		</div>	
 		<div class="row" style="margin-top: 50px;">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
-				<button type="button" 
-						class="btn-5" >VOIR PLUS
-				</button>	
+				<a href="#"><button type="button" class="btn-5" >VOIR PLUS</button></a>
 			</div>
 		</div>
 	</div>
@@ -205,7 +115,7 @@
 						  font-size: 20px;">Postuler avant le 15 Mai 2019
 				</p>
 				<div style="text-align: center;">
-					<a href="#postuler">
+					<a href="index.php?page=postuler">
 						<button type="button" class="btn-6">POSTULER</button>
 					</a>
 					<a href="index.php?page=cooptation">
@@ -218,83 +128,6 @@
 	</div>
 </div>
 
-<section class="container-fluid" style="background:linear-gradient(#ffde59,#ffbd59); padding-bottom: 20px;" id="postuler">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h2 style="font-family:'Raleway', sans-serif;
-						   color: #ffffff;
-						   text-align: center;
-						   font-size: 45px;
-						   font-weight: bold;
-						   padding-bottom: 45px;
-						   padding-top: 60px">Postuler
-				</h2>
-			</div>
-		</div>
-		<div class="row" style="font-family: 'Arimo', sans-serif;color: #222222;">
-			<div class="col-md-6" style="text-align: center;">
-				<input  type="text" placeholder="Nom" style="padding-bottom: 50px;
-															 background:none;
-															 border:none;
-															 text-decoration:none;
-															 font-size: 25px;
-															 font-weight: bold;"><br>
-				<input  type="text" placeholder="Téléphone" style="padding-bottom: 50px;
-															 background:none;
-															 border:none;
-															 text-decoration:none;
-															 font-size: 25px;
-															 font-weight: bold;"><br>
-				<input  type="text" placeholder="CV:" style="padding-bottom: 50px;
-															 background:none;
-															 border:none;
-															 text-decoration:none;
-															 font-size: 25px;
-															 font-weight: bold;" ><br>
-				<input  type="text" placeholder="Copie de diplôme" style="padding-bottom: 50px;
-															 background:none;
-															 border:none;
-															 text-decoration:none;
-															 font-size: 25px;
-															 font-weight: bold;">
-			</div>
-			<div class="col-md-6" style="text-align: center;">
-				<input  type="text" placeholder="Prénom" style="padding-bottom: 50px;
-															 background:none;
-															 border:none;
-															 text-decoration:none;
-															 font-size: 25px;
-															 font-weight: bold;"><br>
-				<input  type="text" placeholder="Adresse email"style="padding-bottom: 50px;
-															 background:none;border: 
-															 none;text-decoration: 
-															 none;
-															 font-size: 25px;
-															 font-weight: bold;"><br>
-				<input  type="text" placeholder="LM:" style="padding-bottom: 50px;
-															 background:none;border: 
-															 none;text-decoration: 
-															 none;
-															 font-size: 25px;
-															 font-weight: bold;"><br>
-				<input  type="text" placeholder="Références" style="padding-bottom: 0px;
-																	background:none;
-																	border: none;
-																	text-decoration: none;
-																	font-size: 25px;
-																	font-weight: bold;">
-			</div>	
-		</div>
-		<div class="row" style="text-align: center;padding-top: 5px;">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<a href="#">
-          			<button class="btn-8" type="button">ENVOYER</button>
-        		</a>
-			</div>
-		</div>
-	</div>
-</section>
 
 <script>
     var xrh=new XMLHttpRequest();
@@ -305,7 +138,7 @@
             var arr=JSON.parse(xrh.response);
 
             for(var i=0;i<arr.length;i++) {
-                document.getElementById("main").innerHTML +='<p>'+arr[i].poste+'</p>'
+                document.getElementById("main").innerHTML +='<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"><div style="background-color:#3b4a61;"><p style="padding-top: 60px;font-size: 15px;">Corban Performance Consulting</p><h2 style="font-family:Lora;font-weight:bold;text-align:center;font-size:40px;padding-top: 20px;">'+arr[i].poste+'</h2><a href="#" style="padding-top: 85px;color:#fff;text-decoration: none;font-weight: bold;float: right;">Détails</a></div></div>'
 
 
                 // '                            <img src="http://127.0.0.1:8000/uploads/'+arr[i].image +'" alt="post">' +
---
+++ application/pages/postuler.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Postuler</title>
+</head>
+<style>
+	.btn-8{
+  text-align: center;
+  font-weight: bold;
+  padding: 10px 40px;
+  font-size: 30px
+  color: #545454;
+  background: #ffffff;
+  border-radius: 50px;
+  border: none;
+}
+.btn-8:hover{
+  font-weight: bold;
+  padding: 10px 40px;
+  font-size: 30px
+  border-radius: 50px;
+  background:#545454;
+  border:2px solid #fff;
+  color: #fff;
+}
+</style>	
+<body>
+<section class="container-fluid" style="background:linear-gradient(#ffde59,#ffbd59); 
+										padding-top:80px;
+										padding-bottom: 30px;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 style="font-family:'Raleway', sans-serif;
+						   color: #ffffff;
+						   text-align: center;
+						   font-size: 45px;
+						   font-weight: bold;
+						   padding-bottom: 45px;
+						   padding-top: 60px">Postuler
+				</h2>
+			</div>
+		</div>
+		<div class="row" style="font-family: 'Arimo', sans-serif;color: #222222;">
+			<div class="col-md-6" style="text-align: center;">
+				<input  type="text" placeholder="Nom" style="padding-bottom: 50px;
+															 background:none;
+															 border:none;
+															 text-decoration:none;
+															 font-size: 25px;
+															 font-weight: bold;"><br>
+				<input  type="text" placeholder="Téléphone" style="padding-bottom: 50px;
+															 background:none;
+															 border:none;
+															 text-decoration:none;
+															 font-size: 25px;
+															 font-weight: bold;"><br>
+				<input  type="text" placeholder="CV:" style="padding-bottom: 50px;
+															 background:none;
+															 border:none;
+															 text-decoration:none;
+															 font-size: 25px;
+															 font-weight: bold;" ><br>
+				<input  type="text" placeholder="Copie de diplôme" style="padding-bottom: 50px;
+															 background:none;
+															 border:none;
+															 text-decoration:none;
+															 font-size: 25px;
+															 font-weight: bold;">
+			</div>
+			<div class="col-md-6" style="text-align: center;">
+				<input  type="text" placeholder="Prénom" style="padding-bottom: 50px;
+															 background:none;
+															 border:none;
+															 text-decoration:none;
+															 font-size: 25px;
+															 font-weight: bold;"><br>
+				<input  type="text" placeholder="Adresse email"style="padding-bottom: 50px;
+															 background:none;border: 
+															 none;text-decoration: 
+															 none;
+															 font-size: 25px;
+															 font-weight: bold;"><br>
+				<input  type="text" placeholder="LM:" style="padding-bottom: 50px;
+															 background:none;border: 
+															 none;text-decoration: 
+															 none;
+															 font-size: 25px;
+															 font-weight: bold;"><br>
+				<input  type="text" placeholder="Références" style="padding-bottom: 0px;
+																	background:none;
+																	border: none;
+																	text-decoration: none;
+																	font-size: 25px;
+																	font-weight: bold;">
+			</div>	
+		</div>
+		<div class="row" style="text-align: center;padding-top: 5px;">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<a href="#">
+          			<button class="btn-8" type="button">ENVOYER</button>
+        		</a>
+			</div>
+		</div>
+	</div>
+</section>
+</body>
+</html>
