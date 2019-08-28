2a53235b05b64c89fda268166311875671535ff7
Mharison <Rakt4.co@gmail.com>
2019-08-28 16:54:15 +0200

    m

---
+++ application/pages/detailsoffre.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Détail Récrutement</title>
+</head>
+<style>
+	.btn-6{
+		border-radius:50px;
+		padding: 8px 40px;
+		color: #ffffff;
+		font-family:'Raleway', sans-serif;
+		font-weight: bold;
+		background: linear-gradient(#00b7ff,#3d5a98,#00b7ff);
+		border:none;
+	}
+	.btn-6:hover{
+		border-radius:50px;
+		padding: 8px 40px;
+		color: #3d5a98;
+		font-family:'Raleway', sans-serif;
+		font-weight: bold;
+		background: #ffffff;
+		border:2px solid #3d5a98;
+	}
+	.detoffre h2{
+		font-weight: bold;
+		font-family: 'Raleway', sans-serif;
+		text-align: center;
+		padding-bottom: 25px;
+		font-size: 28px;
+		padding-top: 25px;
+	}
+	.detoffre p{
+		font-family: 'Arimo', sans-serif;
+		text-align: justify;
+	}
+</style>	
+<body>
+	<section class="container-fluid" style="background:linear-gradient(#00b7ff,#ffffff)">
+		<div class="container">
+			<div class="row"  style="padding-top: 100px;">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 detoffre" id="det">
+					
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="padding-top: 100px; padding-bottom: 25px;">
+				<p style="color: #000;
+						  font-weight: bold;
+						  font-family:'Raleway', sans-serif;
+						  text-align: center;
+						  font-size: 30px;">Postuler avant le 15 Mai 2019
+				</p>
+				<div style="text-align: center;">
+					<a href="index.php?page=postuler">
+						<button type="button" class="btn-6">POSTULER</button>
+					</a>
+					<a href="index.php?page=cooptation">
+						<button type="button" class="btn-6 ">COOPTER</button>	
+					</a>
+						
+				</div>
+			</div>
+			</div>
+		</div>
+	</section>
+<script>
+    var xrh=new XMLHttpRequest();
+    xrh.open('GET','http://127.0.0.1:8000/api/read/of/<?php echo $_GET['id'];  ?>');
+    xrh.onreadystatechange=function () {
+        if (this.readyState === 4 && this.status === 200) {
+
+            var arr=JSON.parse(xrh.response);
+
+          
+                document.getElementById("det").innerHTML +='<h2>'+arr.poste+'</h2><p><strong>Type de contrat :</strong>&nbsp;'+arr.contrat+'</p><p><strong>Date limite:</strong>&nbsp;'+arr.dateLimite+'</p><p><strong>Activités :</strong> &nbsp;'+arr.activite+'</p><p><strong>Mission:</strong><br>'+arr.mission+'</p><p><strong>Profil:</strong>&nbsp;'+arr.profile+'</p><p><strong>Référence :</strong>&nbsp;'+arr.reference+'</p>'
+
+
+                // '                            <img src="http://127.0.0.1:8000/uploads/'+arr[i].image +'" alt="post">' +
+
+
+
+           
+        }
+    };
+    xrh.send();
+
+</script>
+</body>
+</html>
--- application/pages/offres.php
+++ application/pages/offres.php
@@ -51,7 +51,7 @@
       		<img src="assets/images/offre/14.jpg" class="d-block w-100" alt="">
       		<div class="carousel-caption d-none d-md-block" style="bottom: 270px;text-align: center;">
       			<div>
-      				<h4 style="color: #545454; 
+      				<h4 style="color: #fff; 
       						   font-weight: bold;
       						   font-family: 'Raleway', sans-serif;
       						   margin-left: 150px;
@@ -75,60 +75,6 @@
 	</div>
 </div>
 
-<div class="container-fluid" style="background-color:#ffffff;padding: 80px 20px;" id="#">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="color: #222222;">
-				<h2 style="font-weight: bold;
-						  font-family: 'Raleway', sans-serif;
-						  text-align: center;
-						  padding-bottom: 25px;
-						  font-size: 28px;">
-					Lead qualifier (H/F)
-				</h2>
-				<div style="font-family: 'Arimo', sans-serif;text-align: justify;">
-					<p>
-						CORBAN cherche d'urgence un LEAD QUALIFIER (H/F) pour une
-						entreprise d'envergure internationale opérant dans le domaine logistique.
-					</p>
-					<p>MISSION:<br>
-						-étudier, qualifier les différents leads qui se présentent<br>
-						-élaborer offres commerciales.
-					</p>
-					<p>PROFIL:<br>
-						-Bonne connaissance du commerce internationale (idéalement avec des
-						pratiques) attestée par un diplôme reconnu.<br>
-						-Ayant idéalement des vécus en entreprise de logistique internationale
-						(terrestre,maritime, etc.).<br>
-						-Bonne capacité d'adaptation.<br>
-						-Dispose d'un bon niveau en marketing, analyse de marché et de la
-						concurrence [un diplôme en Marketing peut être un plus).
-					</p>
-				</div>
-				
-			</div>
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="padding-top: 150px">
-				<p style="color: #000;
-						  font-weight: bold;
-						  font-family:'Raleway', sans-serif;
-						  text-align: center;
-						  font-size: 20px;">Postuler avant le 15 Mai 2019
-				</p>
-				<div style="text-align: center;">
-					<a href="index.php?page=postuler">
-						<button type="button" class="btn-6">POSTULER</button>
-					</a>
-					<a href="index.php?page=cooptation">
-						<button type="button" class="btn-6 ">COOPTER</button>	
-					</a>
-						
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-
 <script>
     var xrh=new XMLHttpRequest();
     xrh.open('GET','http://127.0.0.1:8000/api/read/offre');
@@ -138,7 +84,7 @@
             var arr=JSON.parse(xrh.response);
 
             for(var i=0;i<arr.length;i++) {
-                document.getElementById("main").innerHTML +='<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"><div style="background-color:#3b4a61; padding:10px 10px;"><p style="padding-top: 60px;font-size: 15px;">Corban Performance Consulting</p><h2 style="font-family:Lora;font-weight:bold;text-align:center;font-size:35px;padding-top: 35px;padding-bottom:100px;">'+arr[i].poste+'</h2><a href="#" style="color:#fff;text-decoration: none;font-weight: bold;font-size:20px;">Détails</a></div></div>'
+                document.getElementById("main").innerHTML +='<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"><div style="background-color:#3b4a61; padding:10px 10px;"><p style="padding-top: 60px;font-size: 15px;">Corban Performance Consulting</p><h2 style="font-family:Lora;font-weight:bold;text-align:center;font-size:35px;padding-top: 35px;padding-bottom:100px;">'+arr[i].poste+'</h2><a href="index.php?page=detailsoffre&id='+arr[i].id+'" style="color:#fff;text-decoration: none;font-weight: bold;font-size:20px;">Détails</a></div></div>'
 
 
                 // '                            <img src="http://127.0.0.1:8000/uploads/'+arr[i].image +'" alt="post">' +
