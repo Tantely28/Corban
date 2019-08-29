1890c68caaeb4fc858cbba7b636874095f879621
jeanmichel07 <tombokely@gmail.com>
2019-08-29 15:26:58 +0300

    client

--- application/menu/menuClient.php
+++ application/menu/menuClient.php
@@ -1,24 +1,30 @@
 <nav class="navbar navbar-expand-lg navbar-dark site_navbar bg-dark site-navbar-light" id="site-navbar">
-      <div class="container">
-        <a class="navbar-brand" href="index.php?page=home"><img src="assets/images/Logo.png" style="width: 50%; height:25%;">  </a>
+    <div class="container">
+        <a class="navbar-brand" href="index.php?page=home"><img src="assets/images/Logo.png" style="width: 89px; height:70px;">  </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#site-nav" aria-controls="site-nav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span> Menu
+            <span class="navbar-toggler-icon"></span> Menu
         </button>
 
         <div class="collapse navbar-collapse" id="site-nav">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active"><a href="index.php?page=home" class="nav-link">Accueil</a></li>
-            <li class="nav-item"><a href="index.php?page=temoignages" class="nav-link">Témoignage</a></li>
-            <li class="nav-item"><a href="index.php?page=ressources" class="nav-link">Ressources</a></li>
-            <li class="nav-item"><a href="index.php?page=service" class="nav-link">Service</a></li>
-            <li class="nav-item"><a href="index.php?page=offres" class="nav-link">Nos offres d'emplois</a></li>
-            <li class="nav-item"><a href="index.php?page=apropos" class="nav-link">A propos</a></li>
+            <ul class="navbar-nav ml-auto" style="font-family: 'Roboto', sans-serif;">
+                <li class="nav-item active"><a href="index.php?page=home" class="nav-link">Accueil</a></li>
+                <li class="nav-item"><a href="index.php?page=temoignages" class="nav-link">Témoignage</a></li>
+                <li class="nav-item"><a href="index.php?page=ressources" class="nav-link">Ressources</a></li>
+                <li class="nav-item"><a href="index.php?page=service" class="nav-link">Service</a></li>
+                <li class="nav-item"><a href="index.php?page=offres" class="nav-link">Offres d'emplois</a></li>
+                <li class="nav-item"><a href="index.php?page=apropos" class="nav-link">A propos</a></li>
+                <li class="nav-item"><a href="index.php?page=contact" class="nav-link">Contact</a></li>
 
-            <li class="nav-item"><a href="index.php?page=contact" class="nav-link">Contact</a></li>
               <li class="nav-item"><a href="logout.php" class="nav-link">Deconnexion</a></li>
           </ul>
+
+
         </div>
+
       </div>
+    <ul style="position: relative; list-style: none;">
+            <a href="#" class="nav-link"><img width="48px" src="assets/images/compte.png"></a>
     </nav>
+<div
 
     <?php include("pages/connexion.php"); ?>
